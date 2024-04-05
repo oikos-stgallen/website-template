1bcf34031d69d0ba63e010626da8bbbeb4c99377
Sau1707 <leonardo@saurwein.ch>
2024-04-06 01:58:52 +0200

    Add Q&A

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let image = '';
+	export let title = '';
+	export let subtitle = '';
+</script>
+
+<div class="hero h-96 mb-12" style={`background-image: url(${image});`}>
+	<div class="hero-overlay bg-opacity-10"></div>
+	<div class="hero-content text-center text-neutral-content">
+		<div class="flex flex-col justify-center">
+			<h1
+				class="text-3xl lg:text-4xl font-bold text-white drop-shadow-xl"
+				style="text-shadow: black 1px 0 10px"
+			>
+				{title}
+			</h1>
+			<p class="text-2lg lg:text-2xl text-white" style="text-shadow: black 1px 0 10px">
+				{subtitle}
+			</p>
+		</div>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	import Footer from '$lib/Footer.svelte';
 </script>
 
+<svelte:head>
+	<title>Oikos</title>
+	<meta name="description" content="Oikos San Gallen website template" />
+</svelte:head>
 <Layout>
 	<slot />
 </Layout>
