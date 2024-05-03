dd830e6b344bde2f0f60e7de93fd45fc2a80cad7
Sau1707 <leonardo@saurwein.ch>
2024-05-03 13:58:45 +0200

    Update +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -30,5 +30,14 @@
 	<h1 class="text-center text-lg lg:text-3xl font-bold mb-4">Contact us</h1>
 	<div class="flex justify-center">
 		<Contact />
+		<iframe
+			src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2699.3171239731623!2d8.49489317606448!3d47.42525940048286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47900b19e300a61b%3A0xd9eed61bccd427f7!2sC%C3%A4sar-Ritz-Strasse%2C%208046%20Z%C3%BCrich!5e0!3m2!1sit!2sch!4v1714334567850!5m2!1sit!2sch"
+			width="600"
+			height="450"
+			style="border:0;"
+			allowfullscreen=""
+			loading="lazy"
+			referrerpolicy="no-referrer-when-downgrade"
+		></iframe>
 	</div>
 </Section>
